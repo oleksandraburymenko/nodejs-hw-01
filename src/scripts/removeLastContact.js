033b17f790d27f5b25373e0c3564424e91e80cbb
oleksandraburymenko <olexandralife@gmail.com>
2024-07-11 17:07:58 +0300

    code

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,24 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from "fs/promises";
+import { getAllContacts } from './getAllContacts.js';
 
-export const removeLastContact = async () => {};
+const removeLastContact = async () => {
+    try {
+        
+        const contacts = await getAllContacts();
+        
+        if (contacts.length > 0) {
+            
+            contacts.pop();
+            
+            await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+            console.log('Last contact removed successfully.');
+        } else {
+            console.log('No contacts to remove.');
+        }
+    } catch (error) {
+        console.error('Something went wrong', error);
+    }
+};
 
-removeLastContact();
+await removeLastContact();
